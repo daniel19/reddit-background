041eb74ddb917d7efaa389863b7c531b6e68cfb0
Daniel Brown <daniel19brown@gmail.com>
2019-10-09 00:35:58 -0500

    Change - Added spec file and updated setup.py
    Started making changes to create an RPM. Used setup.py bdist_rpm to
    create a spec file. Will use tito to build rpm.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
 with open('README.md', 'r') as f:
-    long_desc = f.read()
+    long_desc = ''
+    for line_count, line in enumerate(f):
+        if line_count == 3:
+            short = line
+        long_desc += line
 
 setup(
         name='reddit-background',
+        version='0.0.0.1',
+        description=short,
         long_description=long_desc,
         long_description_content_type='text/markdown',
         packages=find_packages(),
         entry_points={
-            'console_scripts': ['imgur_loader=reddit_background.imgur.imgur_loader:main', 'reddit_background=reddit_background.reddit_background:main']
+            'console_scripts': ['reddit_background=reddit_background.reddit_background:main']
         },
         python_requires='>=3.7', install_requires=['']
 )
