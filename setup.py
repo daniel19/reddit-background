1931b2162839c697923a07ed1b0cbad1e36c9e9e
Daniel Brown <daniel19brown@gmail.com>
2019-12-29 21:01:28 -0600

    Change - Updated to version 0.0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as f:
 
 setup(
         name='reddit-background',
-        version='0.0.0.1',
+        version='0.0.0.2',
         description=short,
         long_description=long_desc,
         long_description_content_type='text/markdown',
