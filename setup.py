64d17fca8771d591164ffae2fed200ce3287a485
Daniel Brown <daniel19brown@gmail.com>
2020-11-20 08:01:46 -0600

    Change - Bumped to version 0.3.0.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as f:
 
 setup(
         name='reddit-background',
-        version='0.2.2',
+        version='0.3.0',
         description=short,
         long_description=long_desc,
         long_description_content_type='text/markdown',
